dc4956f108a25e99293a18bb99ee37d391984b73
Nate <78760239+NateNoNameSOFT@users.noreply.github.com>
2022-05-21 12:49:22 -0700

    update enum basics

--- enums_basics/src/main.rs
+++ enums_basics/src/main.rs
@@ -9,6 +9,33 @@ enum IpAddr {
     V6(String),
 }
 
+#[derive(Debug)]
+enum State {
+    AK, AL, AR, AS, AZ, CA, CO, CT, DC, DE, FL, GA, GU, HI, IA, ID, IL, IN, KS, KY, LA, MA, MD, ME, MI, MN, MO, MP, MS, MT, NC, ND, NE, NH, NJ, NM, NV, NY, OH, OK, OR, PA, PR, RI, SC, SD, TN, TX, UM, UT, VA, VI, VT, WA, WI, WV, WY,
+}
+
+enum Coin {
+    Penny,
+    Nickle,
+    Dime,
+    Quarter(State),
+}
+
+impl Coin {
+    fn value_in_cents(&self) -> u32 {
+        match self {
+            Coin::Penny => 1,
+            Coin::Nickle => 5,
+            Coin::Dime => 10,
+            // Coin::Quarter(WA) <--- State will be set to WA
+            Coin::Quarter(state) => {
+                println!("Quarter is from the state: {:?}", state);
+                25
+            },
+        }
+    }
+}
+
 // this enum has four varients with different types
 // Quit has no data associated with it
 // Move has an anonymous struct inside it
@@ -23,9 +50,7 @@ enum Message {
 
 // enums can also have impl blocks
 impl Message {
-    fn call(&self) {
-
-    }
+    fn call(&self) { }
 }
 
 fn main() {
@@ -41,6 +66,12 @@ fn main() {
 
     let msg = Message::Write(String::from("Hello World"));
     msg.call();
+
+    let coin1 = Coin::Penny;
+    let coin2 = Coin::Quarter(State::WA);
+
+    println!("penny value in cents: {}", coin1.value_in_cents());
+    println!("quarter value in cents: {}", coin2.value_in_cents());
 }
 
 fn route(ip_type: IpAddrKind) {}
